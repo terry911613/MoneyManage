15eb3d1dcbb637c1578ee306330745de4cfb489d
TaiYiLee <terry911613@gmail.com>
2019-04-04 23:51:59 +0800

    Initial Commit

---
+++ MoneyManage/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MoneyManage
+//
+//  Created by 李泰儀 on 2019/4/4.
+//  Copyright © 2019 TerryLee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoneyManage/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MoneyManage
+//
+//  Created by 李泰儀 on 2019/4/4.
+//  Copyright © 2019 TerryLee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
